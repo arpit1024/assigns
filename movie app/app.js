7fb1499d47da9dd9c52bc55be14c37720532ce35
arpit1024 <91615308+arpit1024@users.noreply.github.com>
2021-11-27 22:04:41 +0530

    Add files via upload

---
+++ movie app/app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const connect = ()=>{
+    try{
+        return mongoose.connect("mongodb+srv://arpit321:arpit222@cluster0.epnav.mongodb.net/movieDatabase");
+    }catch(e){
+        console.log(e);
+    }
+};
+
+const movieSchema = new mongoose.Schema({
+    id: {type:Number, required:true},
+    movie_name: {type:String, required:true},
+    movie_genre: {type:String, required:true},
+    production_year: {type:Number, required:true},
+    budget: {type:Number, required:true}
+});
+
+const Movie = mongoose.model("movie", movieSchema);
+
+const app=express();
+app.use(express.json());
+
+app.get('/movies',async(req,res)=>{
+    try{
+        const users=await Movie.find().lean().exec();
+        res.send({users});
+    }
+    catch(e){
+        res.status(500).json({message:e.message, status:"Failed"});
+    }
+})
+
+app.post("/movies", async(req,res)=>{
+    try{
+        const user=await Movie.create(req.body);
+        console.log("inside post");
+        res.status(201).send(user);
+    }catch(e){
+        res.status(500).json({message:e.message, status:"Failed"});
+    }
+})
+
+app.get('/movies/:id', async(req,res)=>{
+    try{
+        const movie = await Movie.findById(req.params.id).lean().exec();
+        res.send({movie});
+    }
+    catch(e){
+        res.status(500).json({message:e.message, status:"Failed"});
+    }
+})
+
+app.patch('/movies/:id', async(req,res)=>{
+    try{
+        const movie=await Movie.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        res.send({movie});
+    }
+    catch(e){
+        res.status(500).json({message:e.message, status: "Failed!"});
+    }
+})
+
+app.delete('/users/:id', async(req,res)=>{
+    try{
+        const movie = await Movie.findByIdAndDelete(req.params.id,{new:true}).lean().exec();
+        res.send({movie});
+    }
+    catch(e){
+        res.status(500).json({ message:e.message, status: "Failed!" });
+    }
+})
+
+app.listen(2345, async ()=>{
+    await connect();
+    console.log("listening...");
+})
