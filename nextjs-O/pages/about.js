46f6ddc01df3e6f7be9802ac930ec744c36f506d
arpit1024 <91615308+arpit1024@users.noreply.github.com>
2022-01-01 17:41:30 +0530

    Add files via upload

---
+++ nextjs-O/pages/about.js
@@ -0,0 +1,4 @@
+import  Link  from "next/link";
+const aboutPage = () => <h1>THIS IS ABOUT</h1>
+
+export default aboutPage;
---
+++ nextjs-O/pages/index.js
@@ -0,0 +1,10 @@
+import Link  from "next/link";
+const IndexPage = () =>
+(
+    <>
+     <h1>INDEX PAGE</h1>
+     <Link href='/about'>GO TO ABOUT PAGE</Link>
+    </>
+)
+
+export default IndexPage;
